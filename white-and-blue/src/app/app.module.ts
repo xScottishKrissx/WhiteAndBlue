4e8853f608b620fc48351dfa0f180e13079b364c
xScottishKrissx <chrisdunne66@gmail.com>
2018-03-07 16:45:42 +0000

    Day 1 - Basic Structure in place

--- white-and-blue/src/app/app.module.ts
+++ white-and-blue/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MainWrapperComponent } from './main-wrapper/main-wrapper.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainWrapperComponent
   ],
   imports: [
     BrowserModule
